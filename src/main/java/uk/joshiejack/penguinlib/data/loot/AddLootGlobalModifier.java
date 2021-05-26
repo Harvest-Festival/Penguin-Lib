5ccf4e1bd56fc042eaf9e17119c7cd13e372934d
joshiejack <joshjackwildman@gmail.com>
2021-05-26 23:59:20 +0100

    Allow multiple drops

--- src/main/java/uk/joshiejack/penguinlib/data/loot/AddLootGlobalModifier.java
+++ src/main/java/uk/joshiejack/penguinlib/data/loot/AddLootGlobalModifier.java
@@ -19,18 +19,21 @@ import java.util.List;
 public class AddLootGlobalModifier extends LootModifier {
     private final List<Item> items = new ArrayList<>();
     private final boolean clearItems;
+    private final int itemCount;
 
-    public AddLootGlobalModifier(ILootCondition[] conditions, List<Item> items, boolean clearItems) {
+    public AddLootGlobalModifier(ILootCondition[] conditions, List<Item> items, boolean clearItems, int itemCount) {
         super(conditions);
         this.items.addAll(items);
         this.clearItems = clearItems;
+        this.itemCount = itemCount;
     }
 
     @Nonnull
     @Override
     protected List<ItemStack> doApply(List<ItemStack> generatedLoot, LootContext context) {
         if (clearItems) generatedLoot.clear();
-        generatedLoot.add(new ItemStack(items.get(context.getRandom().nextInt(items.size()))));
+        for (int i = 0; i < itemCount; i++)
+            generatedLoot.add(new ItemStack(items.get(context.getRandom().nextInt(items.size()))));
         return generatedLoot;
     }
 
@@ -45,10 +48,9 @@ public class AddLootGlobalModifier extends LootModifier {
                     items.add(ForgeRegistries.ITEMS.getValue(new ResourceLocation(item)));
             }
 
-            boolean clear = false;
-            if (object.has("clear_items"))
-                clear = JSONUtils.getAsBoolean(object, "clear_items");
-            return new AddLootGlobalModifier(conditions, items, clear);
+            boolean clear = object.has("clear_items") && JSONUtils.getAsBoolean(object, "clear_items");
+            int itemCount = object.has("item_count") ? JSONUtils.getAsInt(object, "item_count") : 1;
+            return new AddLootGlobalModifier(conditions, items, clear, itemCount);
         }
 
         @Override
@@ -60,6 +62,8 @@ public class AddLootGlobalModifier extends LootModifier {
             object.add("items", array);
             if (instance.clearItems)
                 object.addProperty("clear_items", true);
+            if (instance.itemCount > 1)
+                object.addProperty("item_count", instance.itemCount);
             return object;
         }
     }
