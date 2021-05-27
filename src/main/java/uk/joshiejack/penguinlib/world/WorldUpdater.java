a1055d962992eeec961ecc9951bf1f236f10492d
joshiejack <joshjackwildman@gmail.com>
2021-05-27 15:19:26 +0100

    Re-enable the new day event

---
+++ src/main/java/uk/joshiejack/penguinlib/world/WorldUpdater.java
@@ -0,0 +1,20 @@
+package uk.joshiejack.penguinlib.world;
+
+import net.minecraft.world.server.ServerWorld;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import uk.joshiejack.penguinlib.PenguinLib;
+import uk.joshiejack.penguinlib.events.NewDayEvent;
+import uk.joshiejack.penguinlib.util.helpers.minecraft.TimeHelper;
+
+@Mod.EventBusSubscriber(modid = PenguinLib.MODID)
+public class WorldUpdater {
+    @SubscribeEvent
+    public static void onWorldTick(TickEvent.WorldTickEvent event) {
+        if (!event.world.isClientSide && event.phase == TickEvent.Phase.END && event.world.getDayTime() % TimeHelper.TICKS_PER_DAY == 1) {
+            MinecraftForge.EVENT_BUS.post(new NewDayEvent((ServerWorld) event.world)); //Post the new day event, to update
+        }
+    }
+}
