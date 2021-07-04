baef10bdd9794f8e96d2ef4fa1d467f8e92a525e
joshiejack <joshjackwildman@gmail.com>
2021-07-04 23:30:21 +0100

    Fix crash with books.

--- src/main/java/uk/joshiejack/penguinlib/item/base/BookItem.java
+++ src/main/java/uk/joshiejack/penguinlib/item/base/BookItem.java
@@ -3,6 +3,7 @@ package uk.joshiejack.penguinlib.item.base;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.inventory.container.ContainerType;
+import net.minecraft.inventory.container.IContainerProvider;
 import net.minecraft.inventory.container.SimpleNamedContainerProvider;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -11,14 +12,15 @@ import net.minecraft.util.Hand;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.network.NetworkHooks;
+import uk.joshiejack.penguinlib.inventory.AbstractBookContainer;
 
 import javax.annotation.Nonnull;
 import java.util.function.Supplier;
 
 public class BookItem extends Item {
-    private final Supplier<ContainerType<?>> container;
+    private final Supplier<IContainerProvider> container;
 
-    public BookItem(Properties properties, Supplier<ContainerType<?>> container) {
+    public BookItem(Properties properties, Supplier<IContainerProvider> container) {
         super(properties);
         this.container = container;
     }
@@ -28,7 +30,7 @@ public class BookItem extends Item {
     public ActionResult<ItemStack> use(@Nonnull World world, @Nonnull PlayerEntity player, @Nonnull Hand hand) {
         if (!world.isClientSide) {
             NetworkHooks.openGui((ServerPlayerEntity) player,
-                    new SimpleNamedContainerProvider((id, inv, p) -> container.get().create(id, inv),
+                    new SimpleNamedContainerProvider(container.get(),
                     new TranslationTextComponent(getDescriptionId())));
         }
 
