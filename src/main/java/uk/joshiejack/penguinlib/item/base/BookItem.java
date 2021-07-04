42452cb208fa036d0e0cbbc0be8e78680b160a92
joshiejack <joshjackwildman@gmail.com>
2021-07-04 23:32:47 +0100

    Fix server crash with books.

--- src/main/java/uk/joshiejack/penguinlib/item/base/BookItem.java
+++ src/main/java/uk/joshiejack/penguinlib/item/base/BookItem.java
@@ -3,6 +3,7 @@ package uk.joshiejack.penguinlib.item.base;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.inventory.container.ContainerType;
+import net.minecraft.inventory.container.IContainerProvider;
 import net.minecraft.inventory.container.SimpleNamedContainerProvider;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -16,9 +17,9 @@ import javax.annotation.Nonnull;
 import java.util.function.Supplier;
 
 public class BookItem extends Item {
-    private final Supplier<ContainerType<?>> container;
+    private final Supplier<IContainerProvider> container;
 
-    public BookItem(Properties properties, Supplier<ContainerType<?>> container) {
+    public BookItem(Properties properties, Supplier<IContainerProvider> container) {
         super(properties);
         this.container = container;
     }
@@ -28,7 +29,7 @@ public class BookItem extends Item {
     public ActionResult<ItemStack> use(@Nonnull World world, @Nonnull PlayerEntity player, @Nonnull Hand hand) {
         if (!world.isClientSide) {
             NetworkHooks.openGui((ServerPlayerEntity) player,
-                    new SimpleNamedContainerProvider((id, inv, p) -> container.get().create(id, inv),
+                    new SimpleNamedContainerProvider(container.get(),
                     new TranslationTextComponent(getDescriptionId())));
         }
 
